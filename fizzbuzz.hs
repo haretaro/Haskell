0ac84b0c72c01c9f1bf4e959c471b82965773f0e
haretaro <hoge@example.com>
2015-06-19 23:54:34 +0900

    fixed condition

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,7 +1,8 @@
 fizzbuzz :: Integer -> String
-fizzbuzz n  | mod n 3 == 0 && mod n 5 == 0 = "FizzBuzz"
-            | mod n 3 == 0 = "Fizz"
-	    | mod n 5 == 0 = "Buzz"
-            | otherwise = show n
+fizzbuzz n
+	| mod n 15 == 0 = "FizzBuzz"
+	| mod n 3 == 0 = "Fizz"
+	| mod n 5 == 0 = "Buzz"
+	| otherwise = show n
 
 main =	(putStrLn . unwords . map fizzbuzz) [1..100]
