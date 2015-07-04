e2576f2018b776b91331f69775f58b5c56ca455f
haretaro <frontend.1000@gmail.com>
2015-07-04 16:29:55 +0900

    add get method

--- mylist.hs
+++ mylist.hs
@@ -4,6 +4,13 @@ cons :: [t] -> Node t
 cons [] = Nil
 cons list = (Node (head list) . cons . tail) list
 
+get :: Node t -> Integer ->  t
+get node 0 = datum node
+get node n = get (next node) (n-1)
+
+
 main = do
   (putStrLn . show . cons) [1,2,3]
-  (putStrLn . show . cons) ["the","answer","is","42"]
+  let list = ["the","answer","is","42"]
+  (putStrLn . show . cons) list
+  (putStrLn . show . get (cons list)) 2
