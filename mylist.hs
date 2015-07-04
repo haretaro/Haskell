5e350aa6672ffa68e15f820076d2c80a5696a4cf
haretaro <frontend.1000@gmail.com>
2015-07-04 22:49:50 +0900

    add len method

--- mylist.hs
+++ mylist.hs
@@ -8,9 +8,14 @@ get :: Node t -> Integer ->  t
 get node 0 = datum node
 get node n = get (next node) (n-1)
 
+len :: Node t -> Integer
+len Nil = 0
+len node = 1 + (len . next ) node
+
 
 main = do
   (putStrLn . show . cons) [1,2,3]
-  let list = ["the","answer","is","42"]
-  (putStrLn . show . cons) list
-  (putStrLn . show . get (cons list)) 2
+  let list = cons ["the","answer","is","42"]
+  (putStrLn . show) list
+  (putStrLn . show . get list) 2
+  (putStrLn . show . len) list
