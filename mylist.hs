95be5262f33c202414c0bb8e5c76d27154f4b4b2
haretaro <frontend.1000@gmail.com>
2015-07-05 18:26:13 +0900

    now I understand pattern matching

--- mylist.hs
+++ mylist.hs
@@ -1,7 +1,7 @@
-data Node t = Node{datum::t,next::Node t} | Nil deriving (Show,Eq)
+data Node t = Node{datum::t,next::Node t} | End{datum::t} deriving (Show,Eq)
 
 cons :: [t] -> Node t
-cons [] = Nil
+cons [a] = End a
 cons list = (Node (head list) . cons . tail) list
 
 get :: Node t -> Integer ->  t
@@ -9,13 +9,17 @@ get node 0 = datum node
 get node n = get (next node) (n-1)
 
 len :: Node t -> Integer
-len Nil = 0
+len (End _) = 1
 len node = 1 + (len . next ) node
 
+showData :: Show t => Node t -> String
+showData (End datum) = show datum
+showData (Node datum next) = show datum  ++ "," ++ showData next
 
 main = do
   (putStrLn . show . cons) [1,2,3]
   let list = cons ["the","answer","is","42"]
   (putStrLn . show) list
-  (putStrLn . show . get list) 2
+  (putStrLn . showData) list
+  (putStrLn . show) $ get list 2
   (putStrLn . show . len) list
