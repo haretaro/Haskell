3c86b614c679d690a73fb7aeab4d6dc52f0a0611
haretaro <frontend.1000@gmail.com>
2015-07-16 00:03:42 +0900

    fixed the order of arguments of get function

--- mylist.hs
+++ mylist.hs
@@ -1,19 +1,19 @@
 data Node t = Node{datum::t,next::Node t} | Nil deriving (Show,Eq)
 
 cons :: [t] -> Node t
-cons [a] = End a
+cons [] = Nil
 cons list = (Node (head list) . cons . tail) list
 
-get :: Node t -> Integer ->  t
-get node 0 = datum node
-get node n = get (next node) (n-1)
+get :: Integer -> Node t->  t
+get 0 node= datum node
+get n node= get (n-1) (next node)
 
 len :: Node t -> Integer
-len (End _) = 1
+len Nil = 0
 len node = 1 + (len . next ) node
 
 showData :: Show t => Node t -> String
-showData (End datum) = show datum
+showData Nil = ""
 showData (Node datum next) = show datum  ++ "," ++ showData next
 
 main = do
@@ -21,5 +21,5 @@ main = do
   let list = cons ["the","answer","is","42"]
   (putStrLn . show) list
   (putStrLn . showData) list
-  (putStrLn . show) $ get list 2
+  (putStrLn . show) $ get 2 list
   (putStrLn . show . len) list
